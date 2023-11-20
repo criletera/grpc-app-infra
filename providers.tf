eee455402ec1cb22ba3832231587919889018f4b
Cristian Teran <criletera@gmail.com>
2023-11-19 23:49:05 -0500

    Move terraform state from local to s3.

--- providers.tf
+++ providers.tf
@@ -5,6 +5,15 @@ terraform {
             version = "~> 5.0"
         }
     }
+
+    backend "s3" {
+        bucket                  = "grpc-app-terraform-state"
+        key                     = "grpc-app"
+        region                  = "us-east-1"
+        shared_config_files      = ["/home/cris/.aws/config"]
+        shared_credentials_files = ["/home/cris/.aws/credentials"]
+        profile                  = "test"
+    }
 }
 
 provider "aws" {
